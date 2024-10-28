41d86adfd3d6597db5f102df134e6018b211d6f0
Andrew <filippov@mail.ch>
2024-10-28 17:00:11 +0300

    fix: dart sass warning spam

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+// Fix Dart Sass legacy JS API Deprecation spam
+const cssModernAPI = {
+  preprocessorOptions: {
+    scss: {
+      api: 'modern'
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/loopstudios-landing",
   plugins: [tsconfigPaths(), react()],
+  css: cssModernAPI
 })
