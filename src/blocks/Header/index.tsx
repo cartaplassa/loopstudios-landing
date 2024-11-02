796705264334f0701b0185a9806c3ccd09ee30d2
Andrew <filippov@mail.ch>
2024-11-02 17:44:55 +0300

    style: top nav

--- src/blocks/Header/index.tsx
+++ src/blocks/Header/index.tsx
@@ -4,8 +4,15 @@ import {useState} from "react";
 import "./index.scss";
 import Logo from "/logo.svg";
 import Menu from "/icon-hamburger.svg";
+import Close from "/icon-close.svg";
 import Nav from "@components/Nav";
 import useMinWidth from "@hooks/useMinWidth";
+
+const Motto = () => (
+    <div className="start__wrapper">
+        <h2 className="start__motto">Immersive experiences that deliver</h2>
+    </div>
+);
 
 function Header() {
     const isDesktop = useMinWidth(1000);
@@ -13,25 +20,26 @@ function Header() {
     const toggleNavVisible = () => setNavVisible(!isNavVisible);
 
     return (
-        <div className="start">
+        <div className={"start" + (isNavVisible ? " start--black" : "")}>
             <header className="header">
                 <img src={Logo} alt="Loopstudios logo" />
                 {!isDesktop ? (
-                    <img src={Menu} alt="Menu" onClick={toggleNavVisible} />
+                    <img
+                        src={isNavVisible ? Close : Menu}
+                        alt="Menu"
+                        onClick={toggleNavVisible}
+                    />
                 ) : (
                     <Nav className="start__nav" />
                 )}
             </header>
-            {!isDesktop && (
-                <Collapse isOpened={isNavVisible}>
-                    <Nav className="start__nav" />
-                </Collapse>
+            {isDesktop ? (
+                <Motto />
+            ) : isNavVisible ? (
+                <Nav className="start__nav nav--aligned josefin" />
+            ) : (
+                <Motto />
             )}
-            <div className="start__wrapper">
-                <h2 className="start__motto">
-                    Immersive experiences that deliver
-                </h2>
-            </div>
         </div>
     );
 }
